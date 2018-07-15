34800701399ce58b57eedaeb8a11012f3a85877f
Thomas <tcote19@gmail.com>
2018-07-15 12:05:30 -0700

    Update app.js
    Updated primary functionality to fix bug where entire DB was retrieved upon each new submission. Now only the latest addition is retrieved and displayed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,5 @@
-// Function needed to add the train details of the form group.
 
-// Form fields need to be calculated against first train start.
-
-// Based on train start and frequency determine when the next
-// train will arrive.
-
-// Function should calculate when the next train will arrive, relative to current time.
-
-// The data should be stored in a database(firebase) as admins and users will need to access it anywhere.
-// Data should generate dynmaically on the pageload.
-
+// Firebase config object
 var config = {
     apiKey: "AIzaSyCtbDex0UVg_Y-qCz9qwrVv4BOsGhXQZuY",
     authDomain: "train-scheduler-4c082.firebaseapp.com",
@@ -22,59 +12,54 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var trainNameInput = $("#name-input");
-var trainDestinationInput = $("#destination-input");
-var trainFrequencyInput = $("#frequency-input");
-var trainArrivalInput = $("#arrival-input");
-
-function addTrainDeets(){
-var named = trainNameInput.val().trim();
-var destination = trainDestinationInput.val().trim();
-var arrival = trainArrivalInput.val().trim();
-var frequency = trainFrequencyInput.val().trim();
-
+// submit button to fire primary events
+$("#submit-btn").click(function(){
 	event.preventDefault();
+		var named = $("#name-input").val().trim();
+		var destination = $("#destination-input").val().trim();
+		var arrival = $("#frequency-input").val().trim();
+		var frequency = $("#arrival-input").val().trim();
 
-	var dataObject = {
+// pushes form data into Firebase DB
+	database.ref().push({
 	name : named,
 	dest : destination,
 	time : arrival,
 	freq : frequency 
-	};
+	});
 
-	console.log(dataObject)
-
-	database.ref().push(dataObject);
-
+// resets the forms to be blank after submission
 	$(".form-control").val("");
 
-};
+// DB is queried, latest entry is retrieved, times are beautified using Moment.js and appended into the DOM
+database.ref().once("child_added", function(snapshot, prevChildKey) {
+	var train = snapshot.val();
 
-// submit button click function
-$("#submit-btn").click(function(){
-	addTrainDeets();
-	childFunction();
-});
+	var snapName = train.name;
+	var snapDest = train.dest;
+	var snapFreq = parseInt(train.freq);
+	var firstTrain = train.time;
 
-var childFunction = function(){ 
+	var trainTime = moment().hours(firstTrain.slice(0, 2)).minutes(firstTrain.slice(2, -1));
+	var maxMoment = moment.max(moment(), trainTime);
+	var minsAway, nextArrival;
 
-	database.ref().on("child_added", function(childSnapshot, prevChildKey){
-	console.log(childSnapshot.val());
-
-	var snapName = childSnapshot.val().name;
-	var snapDest = childSnapshot.val().dest;
-	var snapArrival = childSnapshot.val().time;
-	var snapFreq = childSnapshot.val().freq;
-
-	var timePretty = moment.unix(snapArrival).format("hh:mm a");
-
-	console.log(timePretty);
+	//If the first train is later than the current time, sent arrival to the first train time
+	if (maxMoment === trainTime) {
+		nextArrival = trainTime.format("hh:mm A");
+		minsAway = trainTime.diff(moment(), "minutes");
+	} else {
+		var differenceTimes = moment().diff(trainTime, "minutes");
+		var tRemainder = differenceTimes % snapFreq;
+		minsAway = snapFreq - tRemainder;
+		// To calculate the arrival time, add the minsAway to the currrent time
+		nextArrival = moment().add(minsAway, "m").format("hh:mm A");
+	}
 
 	$("#table-main > tbody").append("<tr><td>" + snapName + "</td><td>" + snapDest + "</td><td>"
-	+ snapArrival + "</td><td>" + snapFreq + "</td></tr>");
-
+		+ snapFreq + "</td><td>" + nextArrival + "</td><td>" + minsAway + "</td></tr>");
 });
-};
+});
 
 
 
