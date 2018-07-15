0f9fe829445f752e4f3e2787d454eb1ad1636431
Thomas <tcote19@gmail.com>
2018-07-15 12:14:22 -0700

    Add files via upload
    Adding external config file for security

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCtbDex0UVg_Y-qCz9qwrVv4BOsGhXQZuY",
+    authDomain: "train-scheduler-4c082.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4c082.firebaseio.com",
+    projectId: "train-scheduler-4c082",
+    storageBucket: "",
+    messagingSenderId: "292703910758"
+  };
